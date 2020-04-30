53e5fad1a7c88fd574a11a503d0132708380c6bc
naveddeveloper <naved.ahamad2016@gmail.com>
2020-04-30 18:34:39 +0530

    Relationship

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,8 +3,20 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Reply extends Model
 {
-    //
+    public function question()
+    {
+    	return $this->belognsTo(Question::class);
+    }
+    public function user()
+    {
+    	return $this->belongsTo(User::class);
+    }
+    public function like()
+    {
+    	return $this->hasMany(Like::class);
+    }
 }
